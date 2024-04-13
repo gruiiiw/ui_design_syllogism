0c016c3c205cefcf7d9073730eddb2b5fb1f858e
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-04-13 01:05:43 -0400

    Setting Up lesson Pages

--- server.py
+++ server.py
@@ -14,6 +14,20 @@ app = Flask(__name__)
 def homepage():
    return render_template('homepage.html')
 
+# Lesson Pages
+
+@app.route('/lesson/page1')
+def lesson_page1():
+   return render_template('lesson_page1.html')
+
+@app.route('/lesson/page2')
+def lesson_page2():
+   return render_template('lesson_page2.html')
+
+#Building Block 1 Terms
+@app.route('/lesson/page3')
+def lesson_page3():
+   return render_template('lesson_page3.html')
 
 
 if __name__ == '__main__':
