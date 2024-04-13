11b3d7a431d98bf46732ed696f8f1e486c612165
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-04-13 05:11:58 -0400

    Changing the Pages to rerender

--- server.py
+++ server.py
@@ -17,24 +17,6 @@ def homepage():
 def lesson_page1():
    return render_template('lesson_page1.html')
 
-@app.route('/lesson/page2')
-def lesson_page2():
-   return render_template('lesson_page2.html')
-
-#Building Block 1 Terms
-@app.route('/lesson/page3')
-def lesson_page3():
-   return render_template('lesson_page3.html')
-
-#Building Block 1 Term Drag and Drop
-@app.route('/lesson/page4')
-def lesson_page4():
-   return render_template('lesson_page4.html')
-
-# BUILDING BLOCK 2: PROPOSITIONS
-@app.route('/lesson/page5')
-def lesson_page5():
-   return render_template('lesson_page5.html')
 
 
 
