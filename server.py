d70b7ba834153a6f9b53bc79ab6f13632ba9e247
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-04-12 23:40:21 -0400

    Homepage Setup

--- server.py
+++ server.py
@@ -2,3 +2,14 @@ from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
+
+
+
+
+
+
+# ROUTES
+
+@app.route('/')
+def homepage():
+   return render_template('homepage.html')
