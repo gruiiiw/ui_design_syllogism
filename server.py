9bc85fbb99c6c00dddb708afd431bd3ef6ae19c0
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-04-16 18:44:16 -0400

    Page setup in flask

--- server.py
+++ server.py
@@ -4,7 +4,49 @@ from flask import Response, request, jsonify
 app = Flask(__name__)
 
 
+lessons = {
+   "1":{
+      "lesson_id": "1",
+      "title": "What is a Syllogism",
+      "text": "",
+      "next_lesson":"2"
+   },
 
+   "2":{
+      "lesson_id": "2",
+      "title": "What is a Syllogism",
+      "text": "",
+      "next_lesson":"2"
+   },
+   
+   "3":{
+      "lesson_id": "3",
+      "title": "Building Block 1: Terms",
+      "text": "",
+      "next_lesson":"2"
+   },
+
+   "4":{
+      "lesson_id": "3",
+      "title": "Building Block 2: Propositions",
+      "text": "",
+      "next_lesson":"2"
+   },
+
+   "5":{
+      "lesson_id": "3",
+      "title": "Building Block 2: Propositions",
+      "text": "",
+      "next_lesson":"2"
+   },
+   
+   "6":{
+      "lesson_id": "3",
+      "title": "Building Block 2: Propositions",
+      "text": "",
+      "next_lesson":"2"
+   }
+}
 
 
 # ROUTES
