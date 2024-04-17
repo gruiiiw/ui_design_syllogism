048ab9d86e326e0772ff6f39f9de685f76edb4ae
Ashley Kim <akim20@kentdenver.org>
2024-04-17 00:40:00 -0400

    restructured to have quiz pages

--- server.py
+++ server.py
@@ -7,47 +7,45 @@ app = Flask(__name__)
 
 
 
-
-
 lessons = {
    "1":{
       "lesson_id": "1",
       "title": "What is a Syllogism",
       "text": "Syllogisms: a syllogism is a specific form of argument through which we infer a conclusion on the basis of two premises;\n\nSyllogisms are central to logic, which is the study of correct patterns of reasoning. Logic is all about argument and persuasion.\n\nTo understand syllogisms, we need to understand their parts",
-      "next_lesson":"/learn/2",
-      "prev_lesson":"/"
+      "next":"/learn/2",
+      "prev":"/"
    },
 
    "2":{
       "lesson_id": "2",
       "title": "What is a Syllogism",
       "text": "",
-      "next_lesson":"/learn/3",
-      "prev_lesson":"/learn/1"
+      "next":"/learn/3",
+      "prev":"/learn/1"
    },
    
    "3":{
       "lesson_id": "3",
       "title": "Building Block 1: Terms",
       "text": "Our syllogism definition involved terms and propositions. \n For now, let's look at terms.\n\nTerms: man, ox, runs, wins, yellow, that funny comedian, the book on that shelf, etc.\n\nA term is either a noun, adjective, or verb, and it refers to a category of things.",
-      "next_lesson":"/quiz/1",
-      "prev_lesson":"/learn/2"
+      "next":"/quiz/1",
+      "prev":"/learn/2"
    },
 
    "4":{
       "lesson_id": "4",
       "title": "Building Block 2: Propositions",
       "text": "",
-      "next_lesson":"2",
-      "prev_lesson":""
+      "next":"2",
+      "prev":""
    },
 
    "5":{
       "lesson_id": "5",
       "title": "Building Block 2: Propositions",
       "text": "",
-      "next_lesson":"2",
-      "prev_lesson":""
+      "next":"2",
+      "prev":""
 
    },
    
@@ -55,11 +53,46 @@ lessons = {
       "lesson_id": "6",
       "title": "Building Block 2: Propositions",
       "text": "",
-      "next_lesson":"2",
-      "prev_lesson":"",
+      "next":"2",
+      "prev":"",
       "image":"images\diagram_1.png"
       
    }
+}
+
+quizzes = {
+   "1":{
+      "quiz_id": "1",
+      "title": "What is a Syllogism",
+      "text": "Syllogisms: a syllogism is a specific form of argument through which we infer a conclusion on the basis of two premises;\n\nSyllogisms are central to logic, which is the study of correct patterns of reasoning. Logic is all about argument and persuasion.\n\nTo understand syllogisms, we need to understand their parts",
+      "next":"/learn/2",
+      "prev":"/"
+   },
+
+   "2":{
+      "quiz_id": "1",
+      "title": "What is a Syllogism",
+      "text": "Syllogisms: a syllogism is a specific form of argument through which we infer a conclusion on the basis of two premises;\n\nSyllogisms are central to logic, which is the study of correct patterns of reasoning. Logic is all about argument and persuasion.\n\nTo understand syllogisms, we need to understand their parts",
+      "next":"/learn/2",
+      "prev":"/"
+   },
+
+   "3":{
+      "quiz_id": "1",
+      "title": "What is a Syllogism",
+      "text": "Syllogisms: a syllogism is a specific form of argument through which we infer a conclusion on the basis of two premises;\n\nSyllogisms are central to logic, which is the study of correct patterns of reasoning. Logic is all about argument and persuasion.\n\nTo understand syllogisms, we need to understand their parts",
+      "next":"/learn/2",
+      "prev":"/"
+   },
+
+   "4":{
+      "quiz_id": "1",
+      "title": "What is a Syllogism",
+      "text": "Syllogisms: a syllogism is a specific form of argument through which we infer a conclusion on the basis of two premises;\n\nSyllogisms are central to logic, which is the study of correct patterns of reasoning. Logic is all about argument and persuasion.\n\nTo understand syllogisms, we need to understand their parts",
+      "next":"/learn/2",
+      "prev":"/"
+   }
+
 }
 
 
@@ -70,7 +103,6 @@ def homepage():
    return render_template('homepage.html')
 
 # Lesson Pages
-
 @app.route('/learn/<lesson_id>')
 def learn(lesson_id):
    lesson = lessons[lesson_id]
@@ -78,6 +110,10 @@ def learn(lesson_id):
 
 
 # Quiz Pages - We need to have something to store the score
+@app.route('/quiz/<lesson_id>')
+def quiz(lesson_id):
+   quiz = quizzes[lesson_id]
+   return render_template('quiz.html', lesson=lesson)
 
 
 if __name__ == '__main__':
