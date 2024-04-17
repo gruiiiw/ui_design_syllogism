0a106a4433d6566da3efdecbf88a0d42aca1706c
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-04-17 10:54:22 -0400

    Formatting

--- server.py
+++ server.py
@@ -19,7 +19,8 @@ lessons = {
    "2":{
       "lesson_id": "2",
       "title": "What is a Syllogism",
-      "text": """Here is a more precise definition of a syllogism:
+      "text": """Here is a more precise definition of a syllogism:""",
+      "text1":"""
       Syllogisms: a syllogism is a specific form of argument
       through which we infer a conclusion on the basis of
       two premises, each of which is a proposition.
@@ -32,7 +33,9 @@ lessons = {
    "3":{
       "lesson_id": "3",
       "title": "Building Block 1: Terms",
-      "text": "Our syllogism definition involved terms and propositions. \n For now, let's look at terms.\n\nTerms: man, ox, runs, wins, yellow, that funny comedian, the book on that shelf, etc.\n\nA term is either a noun, adjective, or verb, and it refers to a category of things.",
+      "text": "Our syllogism definition involved terms and propositions. For now, let's look at terms. ",
+      "text1":"""Terms: man, ox, runs, wins, yellow, that funny comedian, the book on that shelf, etc.\n\nA term is either a noun, adjective, or verb, and it refers to a category of things. """,
+
       "next_lesson":"/quiz/1",
       "prev_lesson":"/learn/2"
    },
@@ -59,20 +62,34 @@ lessons = {
 
    "5":{
       "lesson_id": "5",
-      "title": "Building Block 2: Propositions",
-      "text": "",
-      "next_lesson":"2",
-      "prev_lesson":""
+      "title": "BUILDING THE SYLLOGISM",
+      "text": "Now that we know how propositions work, let’s put them together into syllogistic form:",
+      
+      "list1": ["(1) All dogs are animals", "(2) Aristotle is an animal", "(3) Therefore, Aristotle is a dog."],
+      "list2": ["(1) All universities have students", "(2) No high schools are universities", "(3) Therefore, no high schools have students"],
 
+
+      "text3": "Notice how both of these are valid but only the first is sound",
+
+      "next_lesson":"/quiz/3",
+      "prev_lesson":"/quiz/2",
+      
+      "images":["diagram_1.png", "diagram_2.png"]
    },
    
    "6":{
       "lesson_id": "6",
-      "title": "Building Block 2: Propositions",
-      "text": "Now that we know how propositions work, let’s put them together into syllogistic form:",
-      "next_lesson":"2",
-      "prev_lesson":"",
-      "images":["diagram_1.png", "diagram_2.png"]
+      "title": "LOGICAL FALLACIES",
+      "text": "",
+      "text1": "Make sure to not accidentally affirm the consequent:",
+      "list1": ["(1) All dogs are animals", "(2) Aristotle is an animal", "(3) Therefore, Aristotle is a dog."],
+
+      "text2": "or deny the antecedent:",
+      "list2": ["(1) All universities have students", "(2) No high schools are universities", "(3) Therefore, no high schools have students"],
+      
+      "next_lesson":"/quiz/4",
+      "prev_lesson":"/quiz/3",
+      "images":["diagram_3.png", "diagram_4.png"]
 
    }
 }
