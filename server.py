90c1c69e92f04dba290a58151d9f8e82384f4f7f
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-04-18 22:06:41 -0400

    Deleted duplicates

--- server.py
+++ server.py
@@ -2,135 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
 
-
 app = Flask(__name__)
-
-lessons = {
-   "1":{
-      "lesson_id": "1",
-      "title": "What is a Syllogism",
-      "text": "Syllogisms: a syllogism is a specific form of argument through which we infer a conclusion on the basis of two premises;\n\nSyllogisms are central to logic, which is the study of correct patterns of reasoning. Logic is all about argument and persuasion.\n\nTo understand syllogisms, we need to understand their parts",
-      "next_lesson":"/learn/2",
-      "prev_lesson":"/"
-   },
-
-   "2":{
-      "lesson_id": "2",
-      "title": "What is a Syllogism",
-      "text": """Here is a more precise definition of a syllogism:""",
-      "text1":"""
-      Syllogisms: a syllogism is a specific form of argument
-      through which we infer a conclusion on the basis of
-      two premises, each of which is a proposition.
-      Syllogisms are distinguished by the fact that the two
-      premises have exactly one term in common. """,
-      "next_lesson":"/learn/3",
-      "prev_lesson":"/learn/1"
-   },
-   
-   "3":{
-      "lesson_id": "3",
-      "title": "Building Block 1: Terms",
-      "text": "Our syllogism definition involved terms and propositions. For now, let's look at terms. ",
-      "text1":"""Terms: man, ox, runs, wins, yellow, that funny comedian, the book on that shelf, etc.\n\nA term is either a noun, adjective, or verb, and it refers to a category of things. """,
-
-      "next_lesson":"/quiz/1",
-      "prev_lesson":"/learn/2"
-   },
-
-   "4":{
-      "lesson_id": "4",
-      "title": "Building Block 2: Propositions",
-      "text": """A proposition involves connecting two terms,
-      a subject and a predicate , sometimes using
-      a copula, or a connector, such as “is,” “are”,
-      “was”, or “were.”
-
-      Propositions take the form of a declarative
-      sentence and can always be true or false
-
-      Example:
-      “I am hungry. ” is a proposition;
-
-      “Feed me!” is not.""",
-      "next_lesson":"/quiz/2",
-      "prev_lesson":"/quiz/1"
-   },
-
-   "5":{
-      "lesson_id": "5",
-      "title": "BUILDING THE SYLLOGISM",
-      "text": "Now that we know how propositions work, let’s put them together into syllogistic form:",
-      
-      "list1": ["(1) All dogs are animals", "(2) Aristotle is an animal", "(3) Therefore, Aristotle is a dog."],
-      "list2": ["(1) All universities have students", "(2) No high schools are universities", "(3) Therefore, no high schools have students"],
-
-
-      "text3": "Notice how both of these are valid but only the first is sound",
-
-      "next_lesson":"/quiz/3",
-      "prev_lesson":"/quiz/2",
-      
-      "images":["images/diagram_1.png", "images/diagram_2.png"]
-   },
-   
-   "6":{
-      "lesson_id": "6",
-      "title": "LOGICAL FALLACIES",
-      "text": "",
-      "text1": "Make sure to not accidentally affirm the consequent:",
-      "list1": ["(1) All dogs are animals", "(2) Aristotle is an animal", "(3) Therefore, Aristotle is a dog."],
-
-      "text2": "or deny the antecedent:",
-      "list2": ["(1) All universities have students", "(2) No high schools are universities", "(3) Therefore, no high schools have students"],
-      
-      "next_lesson":"/quiz/4",
-      "prev_lesson":"/quiz/3",
-      "images":["images/diagram_3.png", "images/diagram_4.png"]
-   }
-}
-
-quizzes = {
-   "1":{
-      "quiz_id": "1",
-      "title": "Building Block 1: Terms",
-      "text": "Exercise: Drag the following terms into their respective categories",
-      "next":"/learn/4",
-      "prev":"/learn/3"
-   },
-
-   "2":{
-      "quiz_id": "2",
-      "title": "Building Block 2: Propositions",
-      "text": "Create three propositions using the terms and copula provided below",
-      "next":"/learn/5",
-      "prev":"/learn/3"
-   },
-
-   "3":{
-      "quiz_id": "3",
-      "title": "Building the Syllogism",
-      "text": "Build two valid syllogisms using the propositions below. \n Make the first syllogism sound and the second syllogism unsound",
-      "next":"/learn/6",
-      "prev":"/learn/4"
-   },
-
-   "4":{
-      "quiz_id": "4",
-      "title": "Logical Fallacies",
-      "text": "Classify the statements below:",
-      "next":"/quiz/5",
-      "prev":"/learn/5"
-   },
-   "5":{
-      "quiz_id": "5",
-      "title": "Putting it all together",
-      "text": "",
-      "next":"/",
-      "prev":"/learn/5"
-   }
-
-}
 
 lessons = {
    "1":{
@@ -301,7 +173,6 @@ def learn(lesson_id):
 
 # Quiz Pages - We need to have something to store the score
 
-
 @app.route('/quiz/<quiz_id>')
 def quiz(quiz_id):
     quiz = quizzes.get(quiz_id)
@@ -322,8 +193,7 @@ def contains_any(word, terms):
     return any(term in word for term in terms)
 
 
-
-    
+   
 #@app.route('/quiz/page1')
 #def quiz_page1():
 #   return render_template('quiz_page1.html')
