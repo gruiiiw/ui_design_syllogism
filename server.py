e6ffb02bd7453ff84847b2c406f4a522a8e49570
Ashley Kim <akim20@kentdenver.org>
2024-05-08 03:00:57 -0400

    Fixed issue with images not matching text, and fixed font to be consistent throughout!

--- server.py
+++ server.py
@@ -76,7 +76,7 @@ lessons = {
       "title": "Logical Fallacies",
       "text": "",
       "text1": "Make sure to not accidentally affirm the consequent:",
-      "list1": ["(1) All dogs are animals", "(2) Aristotle is an animal", "(3) Therefore, Aristotle is a dog."],
+      "list1": ["(1) All dogs are mammals", "(2) Aristotle is a mammal", "(3) Therefore, Aristotle is a dog."],
 
       "text2": "or deny the antecedent:",
       "list2": ["(1) All universities have students", "(2) No high schools are universities", "(3) Therefore, no high schools have students"],
